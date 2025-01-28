b52eb823b3aab522f1c4295c12ecb9d2095367cb
hady <hadimomin02@gmail.com>
2025-01-29 00:42:49 +0530

    hero seciton v1

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,15 @@
 export default function Home() {
 	return (
 		<>
-			<h1 className="heading">Home</h1>
+			<section className="pink_container">
+				<h1 className="heading">
+					Pitch your start up <br /> connect with entrepreneurs
+				</h1>
+				<p className="sub-heading !max-w-3xl">
+					Submit ideas, Vote on Pitches and Get Noticed in a virtual
+					competition
+				</p>
+			</section>
 		</>
 	);
 }
