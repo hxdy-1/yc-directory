4b040a8b2d098f8dd69b36b74c47579e1b0392e1
hady <hadimomin02@gmail.com>
2025-02-01 16:34:31 +0530

    implemented search

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,12 @@
-export default function Home() {
+import SearchForm from "@/components/SearchForm";
+
+export default async function Home({
+	searchParams,
+}: {
+	searchParams: Promise<{ query?: string }>;
+}) {
+	const query = (await searchParams).query;
+
 	return (
 		<>
 			<section className="pink_container">
@@ -9,6 +17,24 @@ export default function Home() {
 					Submit ideas, Vote on Pitches and Get Noticed in a virtual
 					competition
 				</p>
+
+				<SearchForm query={query} />
+			</section>
+
+			<section className="section_container">
+				<p className="text-30-semibold">
+					{query ? `Search results for "${query}"` : "All Startups"}
+				</p>
+
+				<ul className="mt-7 card_grid">
+					{/* {posts?.length > 0 ? (
+						posts.map((post: StartupTypeCard) => (
+							<StartupCard key={post?._id} post={post} />
+						))
+					) : (
+						<p className="no-results">No startups found</p>
+					)} */}
+				</ul>
 			</section>
 		</>
 	);
